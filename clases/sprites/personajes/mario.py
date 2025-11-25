eadd96a4a5886843a1ce8b1011c4b0f2168653dc
Rafael Pérez de Lema <rafapdlc.work@gmail.com>
2025-11-25 18:02:47 +0100

    Añadir el movimiento y animación de mario

--- clases/sprites/personajes/mario.py
+++ clases/sprites/personajes/mario.py
@@ -3,16 +3,38 @@ import pyxel
 
 class Mario:
 
-    POSICION_1_Y = 109
-    POSICION_1_X = 182
-    POSICION_2_Y = 79
-    POSICION_2_X = 182
-    POSICION_3_Y = 45
-    POSICION_3_X = 182
-
     def __init__(self):
-        self.posicion_y = self.POSICION_1_Y
-        self.posicion_x = self.POSICION_1_X
+        self.posicion = 1
+        self.posicion_1_y = 109
+        self.posicion_2_y = 79
+        self.posicion_3_y = 45
+        self.posicion_x = 182
+        self.paquetePorDerecha = False
+        self.timer_animacion = 0
 
     def draw(self):
-        pyxel.blt(self.posicion_x, self.posicion_y, 0, 32, 0, 16, 16, 0)
+
+        if self.timer_animacion > 0:
+            pyxel.blt(self.posicion_x, self.posicion_1_y, 0, 0, 32, 16, 16, 0)
+
+        else:
+            if self.posicion == 1:
+                pyxel.blt(self.posicion_x, self.posicion_1_y, 0, 32, 0, 16, 16, 0)
+            elif self.posicion == 2:
+                pyxel.blt(self.posicion_x, self.posicion_2_y, 0, 0, 32, 16, 16, 0)
+            elif self.posicion == 3:
+                pyxel.blt(self.posicion_x, self.posicion_3_y, 0, 0, 32, 16, 16, 0)
+
+    def interactuar(self):
+        self.timer_animacion = 30
+
+    def move(self):
+        if self.timer_animacion > 0:
+            self.timer_animacion -= 1
+            return
+
+        if pyxel.btnp(pyxel.KEY_UP) and self.posicion < 3:
+            self.posicion += 1
+
+        if pyxel.btnp(pyxel.KEY_DOWN) and self.posicion > 1:
+            self.posicion -= 1
