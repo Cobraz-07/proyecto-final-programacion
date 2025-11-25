f621add387aac52d200779212cad49863710f79b
Rafael Pérez de Lema <rafapdlc.work@gmail.com>
2025-11-25 18:49:55 +0100

    Cambiar el color de fondo y la posición del camión

--- clases/sprites/objetos/camion.py
+++ clases/sprites/objetos/camion.py
@@ -4,7 +4,7 @@ import pyxel
 class Camion:
     def __init__(self):
         self.pos_x = 2
-        self.pos_y = 56
+        self.pos_y = 54
 
     def draw(self):
-        pyxel.blt(self.pos_x, self.pos_y, 0, 0, 48, 32, 32, 0)
+        pyxel.blt(self.pos_x, self.pos_y, 0, 0, 48, 32, 32, 1)
