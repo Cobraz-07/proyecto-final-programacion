cc410c63094a2384fce1bfddb1ff84d0e2010a7b
Rafael Pérez de Lema <rafapdlc.work@gmail.com>
2025-11-25 12:37:40 +0100

    Añadir todas las posiciones de mario

--- clases/sprites/personajes/mario.py
+++ clases/sprites/personajes/mario.py
@@ -2,8 +2,17 @@ import pyxel
 
 
 class Mario:
+
+    POSICION_1_Y = 109
+    POSICION_1_X = 182
+    POSICION_2_Y = 79
+    POSICION_2_X = 182
+    POSICION_3_Y = 45
+    POSICION_3_X = 182
+
     def __init__(self):
-        self.posicion_y = 100
+        self.posicion_y = self.POSICION_1_Y
+        self.posicion_x = self.POSICION_1_X
 
     def draw(self):
-        pyxel.blt(200, self.posicion_y, 0, 16, 0, 16, 16, 0)
+        pyxel.blt(self.posicion_x, self.posicion_y, 0, 32, 0, 16, 16, 0)
