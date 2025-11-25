abf951c8e406b46944bcfb800627bc7eeaeb98b5
Rafael Pérez de Lema <rafapdlc.work@gmail.com>
2025-11-25 18:02:31 +0100

    Editar las alturas de los paquetes y añadir el movimiento vertical y horizontal

--- clases/sprites/objetos/paquete.py
+++ clases/sprites/objetos/paquete.py
@@ -1,20 +1,63 @@
 import pyxel
+import time
 
 
 class Paquete:
 
     def __init__(self):
-        self.pos_x = 152
-        self.pos_y = 35
-        self.altura1 = 103
-        self.altura2 = 86
-        self.altura3 = 69
-        self.altura4 = 52
-        self.altura5 = 35
-        self.speed = 0.5
+        self.pos_x = 249
+        self.pos_y = 102
+        self.altura1 = 102
+        self.altura2 = 85
+        self.altura3 = 68
+        self.altura4 = 51
+        self.altura5 = 34
+        self.speed = 1
 
     def move(self):
-        pass
+        if self.pos_y == self.altura1:
+            self.pos_x -= self.speed
+        elif self.pos_y == self.altura2:
+            self.pos_x += self.speed
+        elif self.pos_y == self.altura3:
+            self.pos_x -= self.speed
+        elif self.pos_y == self.altura4:
+            self.pos_x += self.speed
+        elif self.pos_y == self.altura5:
+            self.pos_x -= self.speed
+
+    def cambiarAltura(self, mario, luigi):
+        if self.pos_y == self.altura1 and self.pos_x == 205 and mario.posicion == 1:
+            mario.interactuar()
+            self.pos_x = 148
+        elif self.pos_y == self.altura1 and self.pos_x == 205 and mario.posicion != 1:
+            self.pos_x = 170
+            while self.pos_y < 131:
+                self.pos_y += self.speed
+        elif self.pos_y == self.altura1 and self.pos_x == 82 and luigi.posicion == 1:
+            self.pos_y = self.altura2
+        elif self.pos_y == self.altura1 and self.pos_x == 82 and luigi.posicion != 1:
+            self.pos_x = 70
+            while self.pos_y < 131:
+                self.pos_y += self.speed
+        elif self.pos_y == self.altura2 and self.pos_x == 171 and mario.posicion == 2:
+            self.pos_y = self.altura3
+        elif self.pos_y == self.altura2 and self.pos_x == 171 and mario.posicion != 2:
+            self.pos_x = 170
+            while self.pos_y < 131:
+                self.pos_y += self.speed
+        elif self.pos_y == self.altura3 and self.pos_x == 82 and luigi.posicion == 2:
+            self.pos_y = self.altura4
+        elif self.pos_y == self.altura3 and self.pos_x == 82 and luigi.posicion != 2:
+            self.pos_x = 70
+            while self.pos_y < 131:
+                self.pos_y += self.speed
+        elif self.pos_y == self.altura4 and self.pos_x == 171 and mario.posicion == 3:
+            self.pos_y = self.altura5
+        elif self.pos_y == self.altura4 and self.pos_x == 171 and mario.posicion != 3:
+            self.pos_x = 170
+            while self.pos_y < 131:
+                self.pos_y += self.speed
 
     def draw(self):
-        pyxel.blt(self.pos_x, self.pos_y, 0, 0, 0, 16, 16, 0)
+        pyxel.blt(self.pos_x, self.pos_y, 0, 48, 0, 16, 16, 0)
