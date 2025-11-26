653aaffadef45141a35e7084c101782a0c0bcd1d
Rafael Pérez de Lema <rafapdlc.work@gmail.com>
2025-11-26 14:04:24 +0100

    Editar los sprites del camion

--- clases/sprites/objetos/camion.py
+++ clases/sprites/objetos/camion.py
@@ -5,6 +5,8 @@ class Camion:
     def __init__(self):
         self.pos_x = 2
         self.pos_y = 54
+        self.paquetes = 0
 
     def draw(self):
-        pyxel.blt(self.pos_x, self.pos_y, 0, 0, 48, 32, 32, 1)
+        u = self.paquetes * 32
+        pyxel.blt(self.pos_x, self.pos_y, 0, u, 48, 32, 32, 1)
